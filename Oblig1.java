f34764ea233a594b0315bb5c1578f788e59df7ff
Martin Holt Juliussen <martinhj@gmail.com>
2014-02-11 16:40:30 +0100

    Works now. Added some comments on the way further.

--- Oblig1.java
+++ Oblig1.java
@@ -29,13 +29,17 @@
 
 import java.util.Random;
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 //import java.util.Integer;
 // Bedre enn S > 1
 class Oblig1 {
     Random randomg = new Random();
-    static int c = 1000000;
+    static int c = 1000;
     final static int MAX_VALUE = 1000;
-    int ns[], ns2[];
+    ArrayList<Long> itimes = new ArrayList<Long>();
+    ArrayList<Long> atimes = new ArrayList<Long>();
+    int ns[], ns2[], nstemp[];
     public static void main(String [] args) {
         if (args.length > 0) c = Integer.parseInt(args[0]);
         new Oblig1();
@@ -43,18 +47,35 @@ class Oblig1 {
     Oblig1() {
         ns = new int[c];
         ns2 = new int[c];
+        nstemp = new int[c];
         generateNumbers();
-        /* ns = new int[12]; */
-        /* ns[0] = 3; ns[1] = 4; ns[2] = 1; ns[3] = 5; */
-        /* ns[4] = 3; ns[5] = 4; ns[6] = 1; ns[7] = 5; */
-        /* ns[8] = 3; ns[9] = 4; ns[10] = 1; ns[11] = 5; */
         ns2 = ns.clone();
-        iSortSeq(ns, 0, 0);
-        iSortRest(ns);
-        Arrays.sort(ns2);
-        for (int n = 49; n >= 0; n--) {
-            System.out.println(n + " : " + (ns[n] == ns2[c - 1 - n]));
+        nstemp = ns.clone();
+        long time = 0;
+        long startTime = System.nanoTime();
+        for (int i = 0; i < 9; i++) {
+            ns = nstemp.clone();
+            startTime = System.nanoTime();
+            iSortSeq(ns, 0, 0);
+            iSortRest(ns);
+            itimes.add(time = System.nanoTime() - startTime);
         }
+        Collections.sort(itimes);
+        System.out.println("mean: " + (itimes.get(4) / 1000000.0) + "ms");
+        for (int i = 0; i < 9; i++) {
+            ns2 = nstemp.clone();
+            startTime = System.nanoTime();
+            Arrays.sort(ns2);
+            atimes.add(time = System.nanoTime() - startTime);
+        }
+        Collections.sort(atimes);
+        System.out.println("mean: " + (atimes.get(4) / 1000000.0) + "ms");
+        for (int n = 49; n >= 0; n--) {
+            if (ns[n] != ns2[c - 1 - n]) System.out.println(false);
+        }
+        System.out.print("Speedup S: ");
+        System.out.println(
+                String.format("%2.02f", (float)atimes.get(4)/itimes.get(4)));
     }
 void generateNumbers() {
     for (int i = 0; i < c; i++) {
@@ -66,6 +87,16 @@ void generateNumbers() {
  */
 void iSortSeq(int[] a, int v, int h) {
 // v og h brukes ikke nå i denne implementasjonen.
+    /* int i, t; */
+    /* for (int k=v; k < h; k++) { */
+    /*     t = a[k+1]; */
+    /*     i = k; */
+    /*     while (i > v && a[i] > t) { */
+    /*         a[i+1] = a[i]; */
+    /*         i--; */
+    /*     } */
+    /*     a[i+1] = t; */
+    /* } // end for k */
     int j;
     int temp;
     for (int i = 49; i >= 0; i--) {
@@ -92,3 +123,11 @@ void iSortRest(int[] a) {
     }
 }
 } // end class Oblig1
+
+
+// en tråd får en indeks, utfra denne regnes hvilket område som skal jobbes på i array.
+// Skrive om metodene slik at de tar imot argumenter for start og stopp.
+// Ta inn argumenter til programmet for å styre antall tråder og antall tall.
+// iSort tar inn argument for hvor den skal starte, så skal den sortere 50 derfra.
+// iSortRest tar inn samme argument for hvor den skal jobbe i arrayen og jobber mot de
+// 50 første.
