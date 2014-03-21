b20259cb51d84a21d895c8554917a144bc43df98
Martin Holt Juliussen <martinhj@gmail.com>
2014-03-21 22:25:41 +0100

    Finished. Added a report.

--- EratosthenesSil.java
+++ EratosthenesSil.java
@@ -1,14 +1,217 @@
-// TODO:
-// Vise snitt på hvor lang tid det tar å faktorisere (tid / antall 
-// faktoriserte tall (100).
-//
-//
-// I denne oppgaven mangler det noe presisjon. Det vil kunne skje at programmet
-// regner ut flere tall en nødvendig. Dette er for at den forholder seg til hele
-// byter som det lagres i og at det siste tallet som er oppgitt ikke trenger å 
-// være det siste i en byte. Det virker ikke som det skulle gå noe raskere å
-// sjekke for hvilke tall det ikke skulle regnes ut i den byten som inneholder 
-// det siste tallet.
+/* *****************************************************************************\
+ *          Rapport                                                            *
+ * Jeg ser at den sekvensielle faktoriseringen min er langt tregere enn den i  *
+ * fasit i oppgavedokumentet. Og den parallelle er langt tregere enn denne
+ * igjen. Jeg er usikker på hvorfor det er slik og har ikke hatt tid til å se
+ * kompleksiteten i den sekvensielle algoritmen, men om du som retter ser noe
+ * hadde det vært veldig fint å få tilbakemelding på.
+ *
+ * Årsaken til at den parallelle er langt tregere kan være på grunn av at jeg
+ * forsøkte å dele opp primtallene i mer like størrelser. Jeg tok antall
+ * primtall og delte dette på antall tråder. For å få delt opp med riktige
+ * primtall i et område må jeg bruke nextPrime() fram til jeg er innenfor
+ * dette området. Dette bruker jeg nok en stor del av tiden på i store tall
+ * hvor det blir mange hopp.
+ * 
+ * I denne oppgaven mangler det noe presisjon. Det vil kunne skje at programmet
+ * regner ut flere tall en nødvendig. Dette er for at den forholder seg til
+ * hele byter som det lagres i og at det siste tallet som er oppgitt ikke
+ * trenger å være det siste i en byte. Det virker ikke som det skulle gå noe
+ * raskere å sjekke for hvilke tall det ikke skulle regnes ut i den byten som
+ * inneholder det siste tallet.
+ *
+ * Kommentar til målingene.
+ * Her ser det ut til at størrelsen på målingene følger < 10 for
+ * faktoriseringen og litt > 10 for erastothenes sil for de sekvensielle ved
+ * ganging med 10 for N. De parallelle algoritmene følger ikke helt det samme
+ * mønsteret, men dette ser jeg i sammenheng med økt kompleksialitet i å 
+ * starte opp de nye trådene og regne ut start og sluttpunkt.
+ *
+ *
+ * Målinger:
+ * ========
+ *
+ *
+ * TABELL:
+ * .------------------------------------------------------------------------.
+ * |     N      | erseq | erapara | espeedup| facseq | facpara | facspeedup |
+ * | 2000000000 | 26192 |   8337  |   3.13  | 560151 | 1748373 |  0.32      |
+ * |  200000000 |  2062 |    681  |   3.03  |  60232 |  181708 |  0.33      |
+ * |   20000000 |   154 |     38  |   4.05  |   7833 |   19891 |  0.39      |
+ * |    2000000 |    24 |     16  |   1.45  |    774 |    2383 |  0.33      |
+ * `------------------------------------------------------------------------´
+ *
+ *
+ * N = 2000000000, faktoriserer fra 4000000000000000000
+ * (Denne er kjørt bare en gang)
+ * Erastothenes sil sekvensielt: 26102.65ms
+ * Erastothenes sil parallelt:    8337.80ms
+ *                  speedup:      3.130
+ *
+ * Faktorisering sekvensielt:    560151.21ms
+ * Faktorisering parallelt:     1748373.96ms
+ *               speedup:         0.320
+ *
+ *
+ *
+ * SEKVENSIELT:
+ * 100: 3999999999999999900 : 2 * 2 * 3 * 5 * 5 * 89 * 1447 * 1553 * 66666667
+ * 99: 3999999999999999901 : 19 * 2897 * 72670457642207
+ * 98: 3999999999999999902 : 2 * 49965473 * 40027640687
+ * 97: 3999999999999999903 : 3 * 101 * 241 * 54777261958561
+ * 96: 3999999999999999904 : 2 * 2 * 2 * 2 * 2 * 1061 * 117813383600377
+ * 4: 3999999999999999996 : 
+ *        2 * 2 * 3 * 3 * 3 * 3 * 7 * 11 * 13 * 19 * 37 * 52579 * 333667
+ * 3: 3999999999999999997 : 421 * 9501187648456057
+ * 2: 3999999999999999998 : 2 * 432809599 * 4620969601
+ * 1: 3999999999999999999 : 3 * 31 * 64516129 * 666666667
+ * 0: 4000000000000000000 :
+ *        2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2
+ *        * 2 * 2 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 *
+ *        5 * 5 * 5
+ *
+ * PARALLELT:
+ * 100: 3999999999999999900 : 2 * 2 * 3 * 5 * 5 * 89 * 1447 * 1553 * 66666667
+ * 99: 3999999999999999901 : 19 * 2897 * 72670457642207
+ * 98: 3999999999999999902 : 2 * 49965473 * 40027640687
+ * 97: 3999999999999999903 : 3 * 101 * 241 * 54777261958561
+ * 96: 3999999999999999904 : 2 * 2 * 2 * 2 * 2 * 1061 * 117813383600377
+ * 4: 3999999999999999996 : 
+ *        2 * 2 * 3 * 3 * 3 * 3 * 7 * 11 * 13 * 19 * 37 * 52579 * 333667
+ * 3: 3999999999999999997 : 421 * 9501187648456057
+ * 2: 3999999999999999998 : 2 * 432809599 * 4620969601
+ * 1: 3999999999999999999 : 3 * 31 * 64516129 * 666666667
+ * 0: 4000000000000000000 : 
+ *        2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2
+ *        * 2 * 2 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 *
+ *        5 * 5 * 5 
+ *
+ * 
+ * 
+ * 
+ * N = 200000000, faktoriserer fra 40000000000000000
+ * Erastothenes sil sekvensielt: 2062.63ms
+ * Erastothenes sil parallelt:    681.68ms
+ *                  speedup:      3.026      
+ *
+ * Faktorisering sekvensielt:    60232.96ms
+ * Faktorisering parallelt:     181708.08ms
+ *                  speedup:      0.331
+ *
+ * SEKVENSIELT:
+ * 100: 39999999999999900 : 2 * 2 * 3 * 5 * 5 * 7 * 952381 * 19999999
+ * 99: 39999999999999901 : 181 * 229 * 229 * 47237 * 89213
+ * 98: 39999999999999902 : 2 * 16103 * 1242004595417
+ * 97: 39999999999999903 : 3 * 191 * 69808027923211
+ * 96: 39999999999999904 : 2 * 2 * 2 * 2 * 2 * 47 * 7867 * 19843 * 170371
+ * 4: 39999999999999996 : 2 * 2 * 3 * 3 * 11 * 17 * 73 * 101 * 137 * 5882353
+ * 3: 39999999999999997 : 37 * 1081081081081081
+ * 2: 39999999999999998 : 2 * 7 * 47 * 281081 * 216273151
+ * 1: 39999999999999999 : 3 * 89 * 1447 * 1553 * 66666667
+ * 0: 40000000000000000 :
+ *        2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2
+ *        * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5
+ *
+ * PARALLELT:
+ * 100: 39999999999999900 : 2 * 2 * 3 * 5 * 5 * 7 * 952381 * 19999999
+ * 99: 39999999999999901 : 181 * 229 * 229 * 47237 * 89213
+ * 98: 39999999999999902 : 2 * 16103 * 1242004595417
+ * 97: 39999999999999903 : 3 * 191 * 69808027923211
+ * 96: 39999999999999904 : 2 * 2 * 2 * 2 * 2 * 47 * 7867 * 19843 * 170371
+ * 5: 39999999999999995 : 5 * 199999 * 40000200001
+ * 4: 39999999999999996 : 2 * 2 * 3 * 3 * 11 * 17 * 73 * 101 * 137 * 5882353
+ * 3: 39999999999999997 : 37 * 1081081081081081
+ * 2: 39999999999999998 : 2 * 7 * 47 * 281081 * 216273151
+ * 1: 39999999999999999 : 3 * 89 * 1447 * 1553 * 66666667
+ * 0: 40000000000000000 : 
+ *        2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2
+ *        * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5
+ * 
+ *
+ *
+ *     
+ * N = 20000000, faktoriserer fra 400000000000000
+ * Erastothenes sil sekvensielt: 154.95ms
+ * Erastothenes sil parallelt:    38.26ms
+ *                  speedup:      4.050      
+ *
+ * Faktorisering sekvensielt:    7833.09ms
+ * Faktorisering parallelt:     19891.42ms
+ *                  speedup:      0.394
+ *
+ * SEKVENSIELT:
+ * 100: 399999999999900 : 2 * 2 * 3 * 5 * 5 * 17 * 71 * 1657 * 666667
+ * 99: 399999999999901 : 7 * 577 * 99034414459
+ * 98: 399999999999902 : 2 * 5849089 * 34193359
+ * 97: 399999999999903 : 3 * 101 * 1320132013201
+ * 96: 399999999999904 : 2 * 2 * 2 * 2 * 2 * 241 * 379 * 136852823
+ * 4: 399999999999996 : 2 * 2 * 3 * 3 * 11 * 239 * 4649 * 909091
+ * 3: 399999999999997 : 399999999999997
+ * 2: 399999999999998 : 2 * 23 * 8695652173913
+ * 1: 399999999999999 : 3 * 7 * 952381 * 19999999
+ * 0: 400000000000000 :
+ *        2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 5 * 5
+ *        * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5
+ *
+ * PARALLELT:
+ * 100: 399999999999900 : 2 * 2 * 3 * 5 * 5 * 17 * 71 * 1657 * 666667
+ * 99: 399999999999901 : 7 * 577 * 99034414459
+ * 98: 399999999999902 : 2 * 5849089 * 34193359
+ * 97: 399999999999903 : 3 * 101 * 1320132013201
+ * 96: 399999999999904 : 2 * 2 * 2 * 2 * 2 * 241 * 379 * 136852823
+ * 4: 399999999999996 : 2 * 2 * 3 * 3 * 11 * 239 * 4649 * 909091
+ * 3: 399999999999997 : 399999999999997
+ * 2: 399999999999998 : 2 * 23 * 8695652173913
+ * 1: 399999999999999 : 3 * 7 * 952381 * 19999999
+ * 0: 400000000000000 :
+ *        2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 5 * 5
+ *        * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5
+ * 
+ *
+ *
+ *
+ * N = 2000000, faktoriserer fra 4000000000000
+ * Erastothenes sil sekvensielt: 23.79ms
+ * Erastothenes sil parallelt:    16.37ms
+ *                  speedup:      1.454      
+ *
+ * Faktorisering sekvensielt:    774.56ms
+ * Faktorisering parallelt:     2383.58ms
+ *                  speedup:      0.325
+ *
+ * SEKVENSIELT:
+ * 100: 3999999999900 : 2 * 2 * 3 * 5 * 5 * 163 * 409 * 199999
+ * 99: 3999999999901 : 491 * 2207 * 3691273
+ * 98: 3999999999902 : 2 * 23 * 1697 * 1847 * 27743
+ * 97: 3999999999903 : 3 * 1333333333301
+ * 96: 3999999999904 : 2 * 2 * 2 * 2 * 2 * 124999999997
+ * 4: 3999999999996 : 2 * 2 * 3 * 3 * 3 * 7 * 11 * 13 * 37 * 101 * 9901
+ * 3: 3999999999997 : 3877 * 1031725561
+ * 2: 3999999999998 : 2 * 3833 * 521784503
+ * 1: 3999999999999 : 3 * 17 * 71 * 1657 * 666667
+ * 0: 4000000000000 :
+ *        2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 5 * 5 * 5 * 5
+ *        * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5
+ *
+ * PARALLELT:
+ * 100: 3999999999900 : 2 * 2 * 3 * 5 * 5 * 163 * 409 * 199999
+ * 99: 3999999999901 : 491 * 2207 * 3691273
+ * 98: 3999999999902 : 2 * 23 * 1697 * 1847 * 27743
+ * 97: 3999999999903 : 3 * 1333333333301
+ * 96: 3999999999904 : 2 * 2 * 2 * 2 * 2 * 124999999997
+ * 4: 3999999999996 : 2 * 2 * 3 * 3 * 3 * 7 * 11 * 13 * 37 * 101 * 9901
+ * 3: 3999999999997 : 3877 * 1031725561
+ * 2: 3999999999998 : 2 * 3833 * 521784503
+ * 1: 3999999999999 : 3 * 17 * 71 * 1657 * 666667
+ * 0: 4000000000000 :
+ *        2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 5 * 5 * 5 * 5
+ *        * 5 * 5 * 5 * 5 * 5 * 5 * 5 * 5
+ *
+ *
+ *
+ *
+
+
 //
 ///--------------------------------------------------------
 //
@@ -41,6 +244,8 @@ public class EratosthenesSil {
   long faNum;
 
   static int numberOfTests = 1;
+
+  final static int q = Runtime.getRuntime().availableProcessors();
 
   // kanskje trenge du disse
   final int [] bitMask = {1,2,4,8,16,32,64};
@@ -82,23 +287,41 @@ EratosthenesSil (int maxNum) {
  * Runs the tests.
  */
 void runTest(int numberOfTests) {
+  String s;
+  String nl = "\n";
+  double eraSeq, eraPara, facSeq, facPara;
   //System.out.println("Ran with " + numberOfTests + " tests.");
   System.out.print("Erastosthenes Sil sekvensielt: ");
-  System.out.println(runEraSeqTest(numberOfTests) + "ms.");
+  System.out.println((eraSeq = runEraSeqTest(numberOfTests)) + "ms.");
   System.out.print("Number of primes seq: ");
   System.out.println(countAllPrimes());
   System.out.print("Erastosthenes Sil parallelt: ");
-  System.out.println(runEraParTest(numberOfTests) + "ms.");
+  System.out.println((eraPara = runEraParTest(numberOfTests)) + "ms.");
   System.out.print("Number of primes para: ");
   //printAllPrimes();
   System.out.println(countAllPrimes());
   //for (long l: factorize(50)) System.out.print(l + " * ");
   //for (long l: factorize(1999999998)) System.out.print(l + " * ");
   System.out.print("Faktorisering sekvensielt: ");
-  System.out.println(runFacSeqTest(numberOfTests) + "ms.");
+  System.out.println((facSeq = runFacSeqTest(numberOfTests)) + "ms.");
   System.out.println();
   System.out.print("Faktorisering parallelt: ");
-  System.out.println(runFacParTest(numberOfTests) + "ms.");
+  System.out.println((facPara = runFacParTest(numberOfTests)) + "ms.");
+
+  s = "Tider brukt: " + nl;
+  s += "Sekvensiell Erast. sil: " + eraSeq + nl;
+  s += "Parallell Erast. sil: " + eraPara + nl;
+  s += "Sekvensiell faktorisering: " + facSeq + nl;
+  s += "Parallell faktoriserign: " + facPara + nl;
+  s += "Med N på " + maxNum + nl;
+  s += "og faktoriserer " + (long) maxNum * maxNum + nl;
+  s += "Speedup for parallellisert Erastosthenes sil: " + nl;
+  s += eraSeq / eraPara + nl;
+  s += "Speedup for parallellisert faktorisering: " + nl;
+  s += facSeq / facPara + nl;
+  s += "Snitt tid sekvensiell faktorisering: " + nl + (facSeq / 100);
+  s += "Snitt tid parallell faktorisering: " + (facPara / 100);
+  System.out.println(s);
 }
 
 
@@ -158,7 +381,7 @@ double runFacSeqTest(int n) {
   long [] times = new long [n];
   for (int i = 0; i < n; i++) {
     starttime = System.nanoTime();
-    for (long j = (long)maxNum * maxNum - 10; j <= (long)maxNum * maxNum; j++)
+    for (long j = (long)maxNum * maxNum - 100; j <= (long)maxNum * maxNum; j++)
     {
       if (!debug) factorize(j);
       if (debug) {
@@ -168,7 +391,6 @@ double runFacSeqTest(int n) {
       }
     }
     times[i] = System.nanoTime() - starttime;
-    //System.out.println(times[i]/1000000.0);
     Arrays.sort(times);
   }
   return times[numberOfTests/2]/1000000.0;
@@ -186,7 +408,7 @@ double runFacParTest(int n) {
   long [] times = new long [n];
   for (int i = 0; i < n; i++) {
     starttime = System.nanoTime();
-    for (long j = (long)maxNum * maxNum - 10; j <= (long)maxNum * maxNum; j++)
+    for (long j = (long)maxNum * maxNum - 100; j <= (long)maxNum * maxNum; j++)
     {
       if (!debug) factorizeParaRun(j);      
       if (debug) {
@@ -196,7 +418,6 @@ double runFacParTest(int n) {
       }
     }
     times[i] = System.nanoTime() - starttime;
-    //System.out.println(times[i]/1000000.0);
     Arrays.sort(times);
   }
   return times[numberOfTests/2]/1000000.0;
@@ -209,8 +430,6 @@ double runFacParTest(int n) {
  * Split up array to factorize.
  */
 ArrayList<Long> factorizeParaRun(long num) {
-  boolean debug = true;
-  int q = Runtime.getRuntime().availableProcessors();
 	bwait = new CyclicBarrier(q + 1);
 	bfinish = new CyclicBarrier(q + 1);
   int numberOfPrimes = countAllPrimes();
@@ -219,15 +438,16 @@ ArrayList<Long> factorizeParaRun(long num) {
   ArrayList<Long> fac = new ArrayList<Long>();
   ArrayList<ArrayList<Long>> l = new ArrayList<ArrayList<Long>>(q);
   FactorizeRunner a [] = new FactorizeRunner [q];
+
+
   for (int i = 0; i < q; i++) {
     startpoint = numberOfPrimes / q * i;
     if (i == q - 1) stoppoint = numberOfPrimes;
     else stoppoint = (numberOfPrimes / q * (i + 1) - 1);
-    // denne må flyttes til para.
-    //l.add(factorizePara(num, startpoint, stoppoint));
     new Thread(a[i] = new FactorizeRunner(num, startpoint, stoppoint)).start();
-    // legge inn en wait her...
   } // end for i 
+
+
   try {
     bwait.await();
     for (FactorizeRunner ai: a)
@@ -235,33 +455,21 @@ ArrayList<Long> factorizeParaRun(long num) {
     bfinish.await();
   } catch (Exception e) {return null;}
 
+
   long product = 1;
-  // Kjøre denne  etter at run er ferdig kjørt.
-  if (debug) System.out.println("Print out multipliers: ");
   for (ArrayList<Long> list : l) {
     for (long n : list) {
-      if (debug) System.out.print(n + " * ");
       fac.add(n);
       product *= n;
     }
-  }
+  } // end for l
 
   if (product != num) {
-    System.out.println("should add something here.");
     fac.add (num / product);
     product *= num / product;
   }
 
-  if (debug) {
-  System.out.println();
-  System.out.println("Number we were testing:\t" + num);
-  System.out.println("Product:\t\t" + product);
-  System.out.println("Are they the same?");
-  System.out.println(product == num);
-  }
 
-  // Dirty løsning ved å del opp i q -1 antall trårder og så
-  // dele opp det første området i 1/3 og 2/3 i hver sin tråd?
   return fac;
 }
 
@@ -361,7 +569,6 @@ void printAllPrimes(){
  * sparkes i gang.
  */
 void EratosthenesSieveRun() {
-  int q = Runtime.getRuntime().availableProcessors();
 	bwait = new CyclicBarrier(q + 1);
 	bfinish = new CyclicBarrier(q + 1);
 
@@ -412,7 +619,6 @@ void generatePrimesByEratosthenes() {
  * antall tråder).
  */
 void generatePrimesByEratosthenesPara(int index, int startByte, int endByte) {
-  boolean debug = true;
   int firstNum = startByte * 14 + 1;
   int lastNum = endByte * 14 + 14;
 
