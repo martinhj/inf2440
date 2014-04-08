73f676e8fc2f4bb09d818c68574565b7defa8313
Martin Holt Juliussen <martinhj@gmail.com>
2014-04-08 10:42:12 +0200

    Working sequeantial radix sort with test.

--- oblig3/Oblig3.java
+++ oblig3/Oblig3.java
@@ -21,11 +21,14 @@
 /*
  * import java.util.FileWriter
  */
-
+import java.util.Random;
 
 
 
 class Oblig3 {
+  Random randomg = new Random();
+  final static int MAX_VALUE = 1000000;
+
 
 
 
@@ -43,7 +46,14 @@ class Oblig3 {
    * Constructor.
    */
   Oblig3() {
-    pln("Test");
+    pln("***Test***");
+    int n [] = populate(10);
+    for (int i = 0; i < n.length; i++)
+      pln("" + n[i]);
+    radix2(n);
+    pln("*****************");
+    for (int nt : n)
+      pln("" + nt);
   } // end constructor
 
 
@@ -67,65 +77,72 @@ class Oblig3 {
     //if (filewrite) file.
   }
 
-} // end class
 
 
 
-
-
-
-class Temp {
-
-
-
-
-/** 
- * Radix sort with two digits.
- */
-static void radix2(int [] a) {
-  // 2 digit radixSort: a[]
-  int max = a[0], numBit = 2;
-
-  // a) finn max verdi i a[]
-  for (int i = 1 ; i < a.length ; i++)
-    if (a[i] > max) max = a[i];
-
-  while (max >= (1<<numBit) ) numBit++; // antall siffer i max
-
-  // bestem antall bit i siffer1 og siffer2 
-  int bit1 = numBit/2,
-  bit2 = numBit-bit1;
-  int[] b = new int [a.length];
-  radixSort( a, b, bit1, 0); // første siffer fra a[] til b[] radixSort( b,a, bit2, bit1);// andre siffer, tilbake fra b[] til a[]
-} // end radix2
-
-
-
-
-/** 
- * Sort a[] on one digit ; number of bits = maskLen, shiftet up ‘shift’ bits.
- */ 
-static void radixSort ( int [] a, int [] b, int maskLen, int shift){
-  int acumVal = 0, j, n = a.length;
-  int mask = (1<<maskLen) -1;
-  int [] count = new int [mask+1];
-
-  // b) count=the frequency of each radix value in a 
-  for (int i = 0; i < n; i++) {
-    count[(a[i]>> shift) & mask]++; }
-
-  // c) Add up in 'count' - accumulated values 
-  for (int i = 0; i <= mask; i++) {
-    j = count[i];
-    count[i] = acumVal; acumVal += j;
+  /**
+   * Populate an array with random numbers.
+   */
+  int[] populate(int l) {
+    int [] ns = new int [l];
+    for (int i = ns.length - 1; i >= 0; i--) {
+      ns[i] = randomg.nextInt(MAX_VALUE);
+    }
+    return ns;
   }
 
-  // d) move numbers in sorted order a to b 
-  for (int i = 0; i < n; i++) {
-    b[count[(a[i]>>shift) & mask]++] = a[i]; }
-}// end radixSort
+
+
+
+  /** 
+   * Radix sort with two digits.
+   */
+  static void radix2(int [] a) {
+    // 2 digit radixSort: a[]
+    int max = a[0], numBit = 2;
+
+    // a) finn max verdi i a[]
+    for (int i = 1 ; i < a.length ; i++)
+      if (a[i] > max) max = a[i];
+
+    while (max >= (1<<numBit)) numBit++; // antall siffer i max
+
+    // bestem antall bit i siffer1 og siffer2 
+    int bit1 = numBit/2,
+        bit2 = numBit-bit1;
+    int[] b = new int [a.length];
+    radixSort(a, b, bit1, 0); // første siffer fra a[] til b[]
+    radixSort(b, a, bit2, bit1);// andre siffer, tilbake fra b[] til a[]
+
+  } // end radix2
 
 
 
 
-} // End class Temp
+  /** 
+   * Sort a[] on one digit ; number of bits = maskLen, shiftet up ‘shift’ bits.
+   */ 
+  static void radixSort ( int [] a, int [] b, int maskLen, int shift){
+    int acumVal = 0, j, n = a.length;
+    int mask = (1<<maskLen) -1;
+    int [] count = new int [mask+1];
+
+    // b) count=the frequency of each radix value in a 
+    for (int i = 0; i < n; i++) {
+      count[(a[i]>> shift) & mask]++; }
+
+    // c) Add up in 'count' - accumulated values 
+    for (int i = 0; i <= mask; i++) {
+      j = count[i];
+      count[i] = acumVal; acumVal += j;
+    }
+
+    // d) move numbers in sorted order a to b 
+    for (int i = 0; i < n; i++) {
+      b[count[(a[i]>>shift) & mask]++] = a[i]; }
+  }// end radixSort
+
+
+
+
+} // End Oblig3 class
